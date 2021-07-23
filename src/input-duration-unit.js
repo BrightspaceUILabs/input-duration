f1554d15ec3c01405d2ace6722ba78a9842835f6
Jorge Ramirez <Jorge.Ramirez@D2L.com>
2021-07-23 14:01:02 -0400

    Add input-duration prototype

---
+++ src/input-duration-unit.js
@@ -0,0 +1,295 @@
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { classMap } from 'lit-html/directives/class-map.js';
+import LocalizeMixin from './mixins/localize-mixin';
+import { styleMap } from 'lit-html/directives/style-map.js';
+
+const HORIZONTAL_PADDING = 8;
+const VERTICAL_PADDING = 6;
+const INPUT_UNIT_SPACING = 5;
+
+function clamp(value, min, max) {
+	return Math.min(Math.max(value, min), max);
+}
+
+function getDigitCount(value) {
+	return Math.trunc(Math.log10(value)) + 1;
+}
+
+class InputDurationUnit extends LocalizeMixin(LitElement) {
+
+	static get properties() {
+		return {
+			max: {
+				type: Number
+			},
+			min: {
+				type: Number
+			},
+			unitLabel: {
+				type: String,
+				attribute: 'unit-label'
+			},
+			value: {
+				type: String
+			},
+			_unitContainerWidth: { type: Number },
+			_focused: { type: Boolean },
+			_hovered: { type: Boolean }
+		};
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				display: inline-block;
+			}
+			:host([hidden]) {
+				display: none;
+			}
+			.d2l-input-duration-unit {
+				border: none;
+				background: #f9fbff;
+				padding: ${VERTICAL_PADDING}px ${HORIZONTAL_PADDING}px;
+				border-radius: 5px;
+				caret-color: transparent;
+				font-family: inherit;
+				font-size: 0.8rem;
+				font-weight: bold;
+				text-align: right;
+				line-height: 1rem;
+				height: 1rem;
+			}
+			.d2l-input-duration-unit::placeholder {
+				font-weight: bold;
+			}
+			.d2l-input-duration-unit:focus,
+			.d2l-input-duration-unit-focus {
+				background: #006fbe;
+				color: white;
+				outline-style: none;
+				outline-width: 0;
+			}
+			.d2l-input-duration-unit:focus::placeholder,
+			.d2l-input-duration-unit-focus::placeholder {
+				color: white;
+			}
+			.d2l-input-duration-unit-wrapper {
+				display: inline-flex;
+				align-items: center;
+				position: relative;
+			}
+			.d2l-input-duration-unit-label-container {
+				position: absolute;
+				right: ${HORIZONTAL_PADDING}px;
+			}
+			.d2l-input-duration-unit-label-container-focus {
+				color: white;
+			}
+		`;
+	}
+
+	constructor() {
+		super();
+
+		this.max = 99;
+		this.min = 0;
+		this.unitLabel = null;
+		this.value = '';
+		this._unitContainerWidth = 0;
+		this._focused = false;
+	}
+
+	get max() { return this._max; }
+	set max(val) {
+		const oldMax = this._max;
+
+		let newMax = Math.trunc(val); // Only integers are supported
+		newMax = Math.max(newMax, 0); // Negative numbers are not supported
+		this._max = newMax;
+
+		this.requestUpdate('max', oldMax);
+	}
+
+	get min() { return this._min; }
+	set min(val) {
+		const oldMin = this._min;
+
+		let newMin = Math.trunc(val); // Only integers are supported
+		newMin = Math.max(newMin, 0); // Negative numbers are not supported
+		this._min = newMin;
+
+		this.requestUpdate('min', oldMin);
+	}
+
+	get value() { return this._value; }
+	set value(val) {
+		const oldValue = this._value;
+
+		if (!val) {
+			this._value = '';
+		} else {
+			this._value = val.padStart(this.maxDigits, '0');
+		}
+
+		if (this._value !== oldValue) {
+			this.requestUpdate('value', oldValue);
+			this._dispatchChangeEvent(this._value);
+		}
+	}
+
+	get highestMaxDigitsNumber() {
+		return (10 ** this.maxDigits) - 1;
+	}
+
+	get maxDigits() {
+		return getDigitCount(this.max);
+	}
+
+	render() {
+		const inputClass = {
+			'd2l-input-duration-unit': true,
+			'd2l-input-duration-unit-focus': this._focused
+		};
+		const inputStyles = {
+			width: `${this.maxDigits}ch`,
+			paddingRight: `${this._unitContainerWidth + HORIZONTAL_PADDING + INPUT_UNIT_SPACING}px`
+		};
+
+		const unitLabelContainerClass = {
+			'd2l-input-duration-unit-label-container': true,
+			'd2l-input-duration-unit-label-container-focus': this._focused
+		};
+
+		return html`
+			<span class="d2l-input-duration-unit-wrapper">
+				<input
+					type="text"
+					class=${classMap(inputClass)}
+					style=${styleMap(inputStyles)}
+					placeholder=${'0'.repeat(this.maxDigits)}
+					.value=${this.value}
+					@beforeinput=${this._handleBeforeInput}
+					@blur=${this._handleBlur}
+					@focus=${this._handleFocus}
+					@keydown=${this._handleKeyDown}
+				></input>
+				<span class=${classMap(unitLabelContainerClass)}
+					@click=${this.focus}
+				>${this.unitLabel}</span>
+			</span>
+		`;
+	}
+
+	updated(changedProperties) {
+		super.updated(changedProperties);
+
+		changedProperties.forEach((oldVal, prop) => {
+			if (prop === 'unitLabel') {
+				this._updateInputLayout();
+			}
+		});
+	}
+
+	focus() {
+		const elem = this.shadowRoot.querySelector('input');
+		if (elem) elem.focus();
+	}
+
+	_clear() {
+		this.value = '';
+	}
+
+	_decrementValue() {
+		const newValue = Number(this.value || '0') - 1;
+
+		if (newValue >= this.min) {
+			this.value = newValue.toString();
+		}
+	}
+
+	_dispatchChangeEvent(value) {
+		const change = new CustomEvent('change', {
+			detail: {
+				value
+			}
+		});
+		this.dispatchEvent(change);
+	}
+
+	_dispatchNextEvent() {
+		const next = new CustomEvent('next');
+		this.dispatchEvent(next);
+	}
+
+	_dispatchPreviousEvent() {
+		const previous = new CustomEvent('previous');
+		this.dispatchEvent(previous);
+	}
+
+	_enterCharacters(chars) {
+		const newValue = this.value + chars;
+		this.value = newValue.slice(-this.maxDigits);
+	}
+
+	_handleBeforeInput(e) {
+		e.preventDefault();
+
+		if (!e.data) return this._clear();
+
+		let num = Number(e.data);
+
+		if (isNaN(num)) return;
+
+		num = clamp(num, 0, this.highestMaxDigitsNumber);
+
+		this._enterCharacters(num.toString());
+	}
+
+	_handleBlur() {
+		this._focused = false;
+		if (this.value) {
+			this.value = clamp(Number(this.value), this.min, this.max).toString();
+		}
+	}
+
+	_handleFocus(e) {
+		this._focused = true;
+
+		// This prevents the contets of the input from being selected by default when tabbing into it
+		e.target.setSelectionRange(0, 0);
+	}
+
+	_handleKeyDown(e) {
+		switch (e.key) {
+			case 'ArrowUp':
+				this._incrementValue();
+				e.preventDefault();
+				break;
+			case 'ArrowDown':
+				this._decrementValue();
+				e.preventDefault();
+				break;
+			case 'ArrowLeft':
+				this._dispatchPreviousEvent();
+				e.preventDefault();
+				break;
+			case 'ArrowRight':
+				this._dispatchNextEvent();
+				e.preventDefault();
+				break;
+		}
+	}
+
+	_incrementValue() {
+		const newValue = Number(this.value || '0') + 1;
+
+		if (newValue <= this.max) {
+			this.value = newValue.toString();
+		}
+	}
+
+	_updateInputLayout() {
+		this._unitContainerWidth = this.shadowRoot.querySelector('.d2l-input-duration-unit-label-container').getBoundingClientRect().width;
+	}
+}
+customElements.define('d2l-labs-input-duration-unit', InputDurationUnit);
---
+++ src/input-duration.js
@@ -0,0 +1,200 @@
+import './input-duration-unit';
+import './input-duration-wrapper';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import LocalizeMixin from './mixins/localize-mixin';
+import { repeat } from 'lit-html/directives/repeat.js';
+
+const unitTypes = {
+	weeks: 'weeks',
+	days: 'days',
+	hours: 'hours',
+	minutes: 'minutes',
+	seconds: 'seconds'
+};
+
+const untiLabels = {
+	[unitTypes.weeks]: 'week',
+	[unitTypes.days]: 'day',
+	[unitTypes.hours]: 'hour',
+	[unitTypes.minutes]: 'min',
+	[unitTypes.seconds]: 'sec'
+};
+
+const defaultUnitMaximums = {
+	[unitTypes.weeks]: 52,
+	[unitTypes.days]: 7,
+	[unitTypes.hours]: 23,
+	[unitTypes.minutes]: 59,
+	[unitTypes.seconds]: 59
+};
+
+// List of units in order of significance from largest unit to smallest
+const unitsSignificanceOrder = [
+	unitTypes.weeks,
+	unitTypes.days,
+	unitTypes.hours,
+	unitTypes.minutes,
+	unitTypes.seconds
+];
+
+class InputDuration extends LocalizeMixin(LitElement) {
+
+	static get properties() {
+		return {
+			units: {
+				converter: (value) => {
+					if (typeof value !== 'string') return {};
+
+					const unitsMap = value.split(':').reduce((acc, unit) => {
+						acc[unit] = true;
+						return acc;
+					}, {});
+
+					// Only return valid and in the order of significance
+					return unitsSignificanceOrder.reduce((acc, unit) => {
+						if (unitsMap[unit]) acc.push(unit);
+
+						return acc;
+					}, []);
+				}
+			},
+			weeks: {
+				type: Number
+			},
+			days: {
+				type: Number
+			},
+			hours: {
+				type: Number
+			},
+			minutes: {
+				type: Number
+			},
+			seconds: {
+				type: Number
+			},
+			largestUnitMax: {
+				type: Number,
+				attribute: 'largest-unit-max'
+			}
+		};
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				display: inline-block;
+			}
+			:host([hidden]) {
+				display: none;
+			}
+		`;
+	}
+
+	constructor() {
+		super();
+
+		this.units = [];
+		this.largestUnitMax = 99;
+	}
+
+	render() {
+		return html`
+			<d2l-labs-input-duration-wrapper>
+				${repeat(this.units, (unit) => unit, (unit, index) => html`
+					<d2l-labs-input-duration-unit
+						unit=${unit}
+						unit-label=${untiLabels[unit]}
+						max=${index === 0 ? this.largestUnitMax : defaultUnitMaximums[unit]}
+						value=${ifDefined(this._getUnitValue(unit))}
+						@change=${this._genUnitChangeHandler(unit)}
+						@next=${this._genUnitNextHandler(index)}
+						@previous=${this._genUnitPreviousHandler(index)}
+					></d2l-labs-input-duration-unit>
+				`)}
+			</d2l-labs-input-duration-wrapper>
+		`;
+	}
+
+	updated(changedProperties) {
+		super.updated(changedProperties);
+
+		changedProperties.forEach((oldValue, prop) => {
+			if (prop === 'units') {
+				this.unitElements = this.shadowRoot.querySelectorAll('d2l-labs-input-duration-unit');
+			}
+		});
+	}
+
+	_dispatchChange() {
+		const change = new CustomEvent('change', {
+			detail: this.units.reduce((acc, unit) => {
+				acc[unit] = this._getUnitValue(unit);
+				return acc;
+			}, {})
+		});
+		this.dispatchEvent(change);
+	}
+
+	_genUnitChangeHandler(unit) {
+		return (e) => {
+			this._setUnitValue(unit, e.detail.value);
+		};
+	}
+
+	_genUnitNextHandler(currUnitIndex) {
+		return () => {
+			if (currUnitIndex === this.units.length - 1) return; // This is already the last unit
+
+			this.unitElements[currUnitIndex + 1].focus();
+		};
+	}
+
+	_genUnitPreviousHandler(currUnitIndex) {
+		return () => {
+			if (currUnitIndex === 0) return; // This is already the first unit
+
+			this.unitElements[currUnitIndex - 1].focus();
+		};
+	}
+
+	_getUnitValue(unit) {
+		switch (unit) {
+			case unitTypes.weeks:
+				return this.weeks;
+			case unitTypes.days:
+				return this.days;
+			case unitTypes.hours:
+				return this.hours;
+			case unitTypes.minutes:
+				return this.minutes;
+			case unitTypes.seconds:
+				return this.seconds;
+		}
+	}
+
+	_setUnitValue(unit, value) {
+		switch (unit) {
+			case unitTypes.weeks:
+				this.weeks = value;
+				break;
+			case unitTypes.days:
+				this.days = value;
+				break;
+			case unitTypes.hours:
+				this.hours = value;
+				break;
+			case unitTypes.minutes:
+				this.minutes = value;
+				break;
+			case unitTypes.seconds:
+				this.seconds = value;
+				break;
+			default:
+				return; // Skip firing of change event
+		}
+		this._dispatchChange();
+	}
+}
+customElements.define('d2l-labs-input-duration', InputDuration);

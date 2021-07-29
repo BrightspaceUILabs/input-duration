9fd0ca80d4abe26d8ba67115505845e66f44f3fb
Jorge Ramirez <Jorge.Ramirez@D2L.com>
2021-07-29 15:30:22 -0400

    Add new accessibility tests

--- test/input-duration.test.js
+++ test/input-duration.test.js
@@ -1,12 +1,62 @@
 import '../src/input-duration.js';
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
 import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
+
+const fixtures = {
+	basic: html`
+		<d2l-labs-input-duration
+			label="Duration"
+			units="hours:minutes"
+		></d2l-labs-input-duration>
+	`,
+	basicDisabled: html`
+		<d2l-labs-input-duration
+			label="Duration"
+			units="hours:minutes"
+			disabled
+		></d2l-labs-input-duration>
+	`,
+	allUnits: html`
+		<d2l-labs-input-duration
+			label="Duration"
+			units="weeks:days:hours:minutes:seconds"
+		></d2l-labs-input-duration>
+	`,
+	allUnitsDisabled: html`
+		<d2l-labs-input-duration
+			label="Duration"
+			units="weeks:days:hours:minutes:seconds"
+			disabled
+		></d2l-labs-input-duration>
+	`,
+	allUnitsAllValues: html`
+		<d2l-labs-input-duration
+			label="Duration"
+			units="weeks:days:hours:minutes:seconds"
+			largest-unit-max="999"
+			weeks="999"
+			days="7"
+			hours="23"
+			minutes="59"
+			seconds="59"
+		></d2l-labs-input-duration>
+	`
+};
 
 describe('InputDuration', () => {
 
 	describe('accessibility', () => {
-		it('should pass all aXe tests', async() => {
-			const el = await fixture(html`<d2l-labs-input-duration units="hours:minutes" label="Duration"></d2l-labs-input-duration>`);
+		Object.keys(fixtures).forEach((fixtureName) => {
+			it(`[${fixtureName}] should pass all aXe tests`, async() => {
+				const el = await fixture(fixtures[fixtureName]);
+				await expect(el).to.be.accessible();
+			});
+		});
+
+		it('[basic focus] should pass all aXe tests', async() => {
+			const el = await fixture(fixtures.basic);
+			setTimeout(() => el.shadowRoot.querySelector('d2l-labs-input-duration-unit').focus());
+			await oneEvent(el, 'focus');
 			await expect(el).to.be.accessible();
 		});
 	});

e90f4a785994fd40166c1cc28daa1cd87ec8358d
JenkinsReader <resupport@desire2learn.com>
2021-08-25 14:34:27 -0400

    Translation Updates

---
+++ lang/fr-on.js
@@ -0,0 +1,14 @@
+/* eslint quotes: 0 */
+
+export default {
+	"unitNameShort:weeks": "semaine",
+	"unitNameShort:days": "jour",
+	"unitNameShort:hours": "heure",
+	"unitNameShort:minutes": "min.",
+	"unitNameShort:seconds": "sec.",
+	"unitNameFull:weeks": "semaines",
+	"unitNameFull:days": "jours",
+	"unitNameFull:hours": "heures",
+	"unitNameFull:minutes": "minutes",
+	"unitNameFull:seconds": "secondes"
+};
